b84dfb91d14a584e357d0232d49922daebca1bc7
uj <sudo.screenlife@gmail.com>
2017-03-21 10:29:35 +0100

    add codeé

--- src/lib.c
+++ src/lib.c
@@ -1,49 +1,3 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-
-#include "../lib/def.h"
-#include "../lib/nrio.h"
-#include "../lib/nrarith.h"
-#include "../lib/nralloc.h"
-
-#define ELT_X 3
-#define ELT_Y 3
-#define NB_IMAGES 965
-int main(int argc, char const *argv[]) {
-
-  //save_nivgris();
-  //detect_mvt(40);
-  mean_temp();
-  return 0;
-}
-
-void prisemain() {
-  long nrh,nrl,nch,ncl;
-  byte **R, **R2;
-  int i, j;
-
-  R = LoadPGM_bmatrix("../res/Test/carreTrou.pgm",&nrl,&nrh,&ncl,&nch);
-  R2 = bmatrix(nrl, nrh, ncl, nch);
-
-  dilate(R, R2, nrl, nrh, ncl, nch);
-  dilate(R2, R, nrl, nrh, ncl, nch);
-  dilate(R, R2, nrl, nrh, ncl, nch);
-  dilate(R2, R, nrl, nrh, ncl, nch);
-  dilate(R, R2, nrl, nrh, ncl, nch);
-  dilate(R2, R, nrl, nrh, ncl, nch);
-  dilate(R, R2, nrl, nrh, ncl, nch);
-  dilate(R2, R, nrl, nrh, ncl, nch);
-  dilate(R, R2, nrl, nrh, ncl, nch);
-  dilate(R2, R, nrl, nrh, ncl, nch);
-  dilate(R, R2, nrl, nrh, ncl, nch);
-
-  SavePGM_bmatrix(R2,nrl,nrh,ncl,nch,"./prisemain.pgm");
-  free_bmatrix(R, nrl, nrh, ncl, nch);
-  free_bmatrix(R2, nrl, nrh, ncl, nch);
-}
-
-//TODO: Cleanup, do with all the images
 void detect_mvt(int seuil) {
   long nrl, nrh, ncl, nch;
   byte **res;
@@ -86,96 +40,37 @@ void detect_mvt(int seuil) {
   }
 }
 
-/*void mean_temp() {
+void mean_temp() {
   int nb_of_images = 100;
   int i, j, k;
   long nrl, nrh, ncl, nch;
-  rgb8 **cur = LoadPPM_rgb8matrix("../res/Sequences/Fomd/ppm/fomd001.ppm", &nrl, &nrh, &ncl, &nch);
-  long sum_r[ncl][nch];
-  long sum_g[ncl][nch];
-  long sum_b[ncl][nch];
-  //memset( sum_r, 0, ncl*nch*sizeof(int) );
-  //memset( sum_g, 0, ncl*nch*sizeof(int) );
-  //memset( sum_b, 0, ncl*nch*sizeof(int) );
-  my_zero_matrix(sum_r, ncl, nch);
-  my_zero_matrix(sum_g, ncl, nch);
-  my_zero_matrix(sum_b, ncl, nch);
-  rgb8 **mean = rgb8matrix(nrl, nrh, ncl, nch);
-  char *img_prefix = "../res/Sequences/Fomd/ppm/fomd";
-  char *img_suffix = ".ppm";
+  byte **cur = LoadPGM_bmatrix("../res/Sequences/Fomd/pgm/fomd001.pgm", &nrl, &nrh, &ncl, &nch);
+  long sum[ncl][nch];
+  memset( sum, 0, ncl*nch*sizeof(int) );
+  my_zero_matrix(sum, ncl, nch);
+  byte **mean = bmatrix(nrl, nrh, ncl, nch);
+  char *img_prefix = "../res/Sequences/Fomd/pgm/fomd";
+  char *img_suffix = ".pgm";
   char img_path[50];
 
   for(i=0; i<nb_of_images; i++) {
     sprintf(img_path, "%s%.3d%s", img_prefix, i+1, img_suffix);
-    cur = LoadPPM_rgb8matrix(img_path, &nrl, &nrh, &ncl, &nch);
+    cur = LoadPGM_bmatrix(img_path, &nrl, &nrh, &ncl, &nch);
     for(j = 0; j<nrh; j++) {
       for(k = 0; k<nch; k++) {
-        sum_r[j][k] += cur[j][k].r;
-        sum_g[j][k] += cur[j][k].g;
-        sum_b[j][k] += cur[j][k].b;
+        sum[j][k] += cur[j][k];
         //mean[j][k] += (byte)((float)cur[j][k] / (float) nb_of_images);
       }
     }
   }
-
-  for(j = 0; j<nrh; j++) {
-    for(k = 0; k<nch; k++) {
-      mean[j][k].r = sum_r[j][k] / nb_of_images;
-      mean[j][k].g = sum_g[j][k] / nb_of_images;
-      mean[j][k].b = sum_b[j][k] / nb_of_images;
-    }
-  }
-  SavePPM_rgb8matrix(mean, nrl, nrh, ncl, nch, "../res/results/detection/mean_temp.ppm");
-  free_rgb8matrix(cur, nrl, nrh, ncl, nch);
-}*/
-
-void mean_temp() {
-
-  int nb_of_images = NB_IMAGES;
-  long nrl, nrh, ncl, nch;
-  rgb8 **cur = LoadPPM_rgb8matrix("../res/Sequences/Fomd/ppm/fomd001.ppm", &nrl, &nrh, &ncl, &nch);
-  int i, j, k;
-
-  rgb8 **mean = rgb8matrix(nrl, nrh, ncl, nch);
-  char *img_prefix = "../res/Sequences/Fomd/ppm/fomd";
-  char *img_suffix = ".ppm";
-  char img_path[50];
-
-  long sum_r[nrh][nch];
-  long sum_g[nrh][nch];
-  long sum_b[nrh][nch];
-  memset( sum_r, 0, ncl*nch*sizeof(int) );
-  memset( sum_g, 0, ncl*nch*sizeof(int) );
-  memset( sum_b, 0, ncl*nch*sizeof(int) );
-
-  for(i=0; i<nb_of_images; i++) {
-
-    sprintf(img_path, "%s%.3d%s", img_prefix, i+1, img_suffix);
-    cur = LoadPPM_rgb8matrix(img_path, &nrl, &nrh, &ncl, &nch);
-
-
-    for(j=0; j<nrh; j++) {
-      for(k=0; k<nch; k++) {
-        sum_r[j][k] += cur[j][k].r;
-        sum_g[j][k] += cur[j][k].g;
-        sum_b[j][k] += cur[j][k].b;
-      }
-    }
-  }
-
-  for(j=0; j<nrh; j++) {
-    for(k=0; k<nch; k++) {
-      mean[j][k].r = sum_r[j][k] / nb_of_images;
-      mean[j][k].g = sum_g[j][k] / nb_of_images;
-      mean[j][k].b = sum_b[j][k] / nb_of_images;
-    }
-  }
-  SavePPM_rgb8matrix(mean, nrl, nrh, ncl, nch, "../res/results/detection/mean_temp.ppm");
+  SavePGM_bmatrix(mean, nrl, nrh, ncl, nch, "../res/results/detection/mean_temp.pgm");
+  free_bmatrix(cur, nrl, nrh, ncl, nch);
 }
 
 void mean_med() {
   //TODO
 }
+
 //TODO: struct for structuring element
 void erode(byte **from, byte **to, long nrl, long nrh, long ncl, long nch) {
   int i, j;
@@ -272,7 +167,7 @@ void print_elt(int elt[ELT_X][ELT_Y]) {
   }
 }
 
-void my_zero_matrix(long **matrix, long x, long y) {
+void my_zero_matrix(int **matrix, int x, int y) {
   int i, j;
   for(i=0; i<x; i++) {
     for(j=0; j<y; j++) {
